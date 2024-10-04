a8119e8ee209e6ef5da967df134eff33b4c97386
ezzabdalrady <abdalmoezabdelrady@gmail.com>
2024-10-04 15:43:03 +0300

    first commit

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -17,7 +17,7 @@ const LatestCollection = () => {
         <div className='text-center py-8 text-3xl'>
             <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
             <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi voluptatem amet est hic nisi architecto 
+            Discover the latest clothes designed to add an aesthetic touch to your look.
             </p>
         </div>
         
